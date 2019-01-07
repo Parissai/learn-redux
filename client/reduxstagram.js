283e420c74b484ef5487a00d4a4a4576db6e97c0
Parissai <parissa.jamali@gmail.com>
2019-01-07 21:42:06 +0000

    Connect actions to Main component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 
 import store, { history } from './store'
 
-import Main from './components/Main';
+import App from './components/App';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 
@@ -14,7 +14,7 @@ import './styles/style.styl';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path='/' component={Main}>
+      <Route path='/' component={App}>
         <IndexRoute component={PhotoGrid} />
         <Route path='/view/:postId' component={Single} />
       </Route>
