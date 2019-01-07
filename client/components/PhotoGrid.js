e47110d56f57c419d96cfa27ce14adc59f9f2e13
Parissai <parissa.jamali@gmail.com>
2019-01-07 23:33:24 +0000

    Display PhotoGrid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const PhotoGrid = () => (
-  <div>PhotoGrid!</div>
+import Photo from './Photo';
+
+const PhotoGrid = ({ posts, ...props }) => (
+  <div className="photo-grid">
+    {posts.map((post, i) => <Photo key={i} index={i} post={post} {...props} />)}
+  </div>
 )
 
 export default PhotoGrid;
