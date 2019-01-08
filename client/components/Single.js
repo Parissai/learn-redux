4272fa55f313e6602c9906d9801097058593731d
Parissai <parissa.jamali@gmail.com>
2019-01-08 02:13:24 +0000

    Display Single

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const Single = () => (
-  <div className='single-photo'>Single!</div>
-)
+import Photo from './Photo';
+import Comment from './Comment';
+
+const Single = ({ ...props }) => {
+  const { postId } = props.params;
+  const i = props.posts.findIndex((post) => post.code === postId);
+  const post = props.posts[i];
+  const comments = props.comments[postId] || [];
+
+  return (
+    <div className='single-photo'>
+      <Photo index={i} post={post} {...props} />
+      <Comment comments={comments} />
+    </div>
+  )
+}
+
 
 export default Single;
